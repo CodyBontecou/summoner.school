e674ad12fef5131d25e3e0e39a7b0f48f1f39f35
Cody Bontecou <bontecouc@gmail.com>
2021-04-04 15:33:37 -1000

    meta added for header color as well as favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,23 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'theme-color',
+        name: 'theme-color',
+        content: '#6C5ECF',
+      },
+      {
+        hid: 'msapplication-navbutton-color',
+        name: 'msapplication-navbutton-color',
+        content: '#6C5ECF',
+      },
+      {
+        hid: 'apple-mobile-web-app-status-bar-style',
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: '#6C5ECF',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.png' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
